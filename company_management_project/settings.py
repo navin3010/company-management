22ac5ebffac6e7d83a9528f836e9f932b4519b5b
n-a-v-e-e-n-s <naveen.s@techjays.com>
2022-03-01 16:54:18 +0530

    db changed to sqlite

--- company_management_project/settings.py
+++ company_management_project/settings.py
@@ -80,24 +80,24 @@ WSGI_APPLICATION = 'company_management_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'company-details',
-        'USER': 'postgres',
-        'PASSWORD': 'navinsdb',
-        'HOST': '192.168.0.102',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+
+#     'default': {
+
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'company-details',
+#         'USER': 'postgres',
+#         'PASSWORD': 'navinsdb',
+#         'HOST': '192.168.0.102',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
