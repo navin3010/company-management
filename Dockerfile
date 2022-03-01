cb9f23b5320c9ef8d62d5d801826ee2d6b366cf2
n-a-v-e-e-n-s <naveen.s@techjays.com>
2022-03-01 12:05:29 +0530

    stage 1

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3
+
+ENV PYTHONUNBUFFERED 1
+
+RUN mkdir /code
+
+WORKDIR /code
+
+COPY . /code/
+
+RUN python -m pip install --upgrade pip
+
+RUN pip install -r requirements.txt
--- company_management_project/settings.py
+++ company_management_project/settings.py
@@ -77,27 +77,27 @@ TEMPLATES = [
 WSGI_APPLICATION = 'company_management_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+Database
+https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'company-details',
-        'USER': 'postgres',
-        'PASSWORD': 'navinsdb',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+
+#     'default': {
+
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'company-details',
+#         'USER': 'postgres',
+#         'PASSWORD': 'navinsdb',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 
